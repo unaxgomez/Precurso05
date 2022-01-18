c22a02bf0642ec790b2b398115e6a113ed4f8781
Unax <92988157+unaxgomez@users.noreply.github.com>
2022-01-18 20:36:42 +0100

    Add files via upload

---
+++ 04_Ejemplo_ValorInicial_0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _04_Ejemplo_ValorInicial_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[3];
+            string[] palabras = new string[2];
+            Console.WriteLine("Mostraremos el valor almacenado en la posición 0 " + numeros[0] + "  en la posición 1  " + numeros[1] + " y en la posición  de 2  "  + numeros[2] + " del array números");
+            Console.WriteLine("Mostraremos el valor almacenado en la posición 0 " + palabras[0] + "  y en la posición 1  " + palabras[1] +  " del array palabras");
+        }
+    }
+}
---
+++ 04_Ejercicio_Array1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Crear un array de 3 ints. Asignar un valor a cada uno de las 3 posiciones. Mostrar los 3 valores en pantalla.
+ */
+using System;
+
+namespace _04_Ejercicio_Array1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cosas = new int[3];
+
+            int caramelo0 = cosas[0];
+            int caramelo1 = cosas[1];
+            int caramelo2 = cosas[2];
+
+            cosas[0] = 3;
+            cosas[1] = 5;
+            cosas[2] = 7;
+
+            Console.WriteLine(cosas[0]);
+            Console.WriteLine(cosas[1]);
+            Console.WriteLine(cosas[2]);
+
+            //Introduce aquí tu código
+        }
+    }
+}
