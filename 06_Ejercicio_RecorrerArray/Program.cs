1b83c2bb67ece188e0ab9c0bc9ba5695c87bc41e
Unax <92988157+unaxgomez@users.noreply.github.com>
2022-01-18 20:37:39 +0100

    Add files via upload

---
+++ 06_Ejercicio_RecorrerArray/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Crear un array de 10 ints. Asignar un valor a cada uno de las 10 posiciones. Mostrar los 10 valores en pantalla.
+ */
+using System;
+
+namespace _06_Ejercicio_RecorrerArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] casa = new int[10];
+
+            int casa0 = casa[0];
+            int casa1 = casa[1];
+            int casa2 = casa[2];
+            int casa3 = casa[3];
+            int casa4 = casa[4];
+            int casa5 = casa[5];
+            int casa6 = casa[6];
+            int casa7 = casa[7];
+            int casa8 = casa[8];
+            int casa9 = casa[9];
+
+            casa[0] = 4;
+            casa[1] = 5;
+            casa[2] = 7;
+            casa[3] = 12;
+            casa[4] = 66;
+            casa[5] = 9;
+            casa[6] = 8;
+            casa[7] = 1;
+            casa[8] = 2;
+            casa[9] = 3;
+
+            Console.WriteLine(casa[0] + casa[1] + casa[2] + casa[3] + casa[4] + casa[5] + casa[6] + casa[7] + casa[8] + casa[9]);
+
+
+
+            //Introduce aquí tu código
+        }
+    }
+}
---
+++ 08_Ejercicio_Length/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Crear un array de 10 ints,  asigna un valor a cada uno de las 10 posiciones y finalmente muestra los 10 valores en pantalla.
+ */
+using System;
+
+namespace _08_Ejercicio_Length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] cosas = new int[10];
+
+            int caramelo0 = cosas[0];
+            int caramelo1 = cosas[1];
+            int caramelo2 = cosas[2];
+            int caramelo3 = cosas[3];
+            int caramelo4 = cosas[4];
+            int caramelo5 = cosas[5];
+            int caramelo6 = cosas[6];
+            int caramelo7 = cosas[7];
+            int caramelo8 = cosas[8];
+            int caramelo9 = cosas[9];
+            
+            cosas[0] = 3;
+            cosas[1] = 5;
+            cosas[2] = 7;
+            cosas[3] = 9;
+            cosas[4] = 11;
+            cosas[5] = 13;
+            cosas[6] = 15;
+            cosas[7] = 17;
+            cosas[8] = 19;
+            cosas[9] = 21;
+            
+            Console.WriteLine(cosas[0]);
+            Console.WriteLine(cosas[1]);
+            Console.WriteLine(cosas[2]);
+            Console.WriteLine(cosas[3]);
+            Console.WriteLine(cosas[4]);
+            Console.WriteLine(cosas[5]);
+            Console.WriteLine(cosas[6]);
+            Console.WriteLine(cosas[7]);
+            Console.WriteLine(cosas[8]);
+            Console.WriteLine(cosas[9]);
+
+
+
+
+            //Introduce aquí tu código
+        }
+    }
+}
